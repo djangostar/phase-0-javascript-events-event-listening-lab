e37e72b90c5e38fb891948d85a653505586968ce
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2022-01-19 20:19:56 -0800

    commit tree ddac25242c52cdfb1c931b83f48b0bed1c1a209f

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+//Chained arrow function
 function addingEventListener() {
+  document.getElementById('input').addEventListener('click', () => alert('I was clicked!'))
 }
+//Abstracted out
+
+//function addingEventListener() {
+//  const clickMe = document.getElementById('input')
+//  clickMe.addEventListener('click', clickAlert)
+//  function clickAlert() {
+//    alert('I was clicked!')
+//  }
+//}
